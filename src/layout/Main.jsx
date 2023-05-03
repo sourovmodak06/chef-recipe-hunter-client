e8de08c30e373bbf675c4bee23cd3e15153f09c2
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-04 00:41:42 +0600

    spinner section added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/footer/Footer';
+import React from "react";
+import { Outlet, useNavigation } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/footer/Footer";
 
 const Main = () => {
-    return (
-        <div className='min-h-[100vh] flex flex-col'>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const navigation = useNavigation();
+  return (
+    <div className="min-h-[100vh] flex flex-col">
+      <Navbar></Navbar>
+      <div>
+        {navigation.state === "loading" && (
+          <div className="h-[100vh] flex justify-center items-center ">
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+          </div>
+        )}
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
