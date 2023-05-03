10a5931ba94d0ceca5f95980a50cbd64505a8d58
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-03 11:58:48 +0600

    data loaded vercel

--- src/components/Home/cards/ChefInfo.jsx
+++ src/components/Home/cards/ChefInfo.jsx
@@ -1,68 +1,15 @@
 import React from "react";
 import { FaHeart } from "react-icons/fa";
+import { useLoaderData } from "react-router-dom";
 
 const ChefInfo = () => {
-  const chefs = [
-    {
-      id: 1,
-      chef_picture:
-        "https://images.unsplash.com/photo-1512485800893-b08ec1ea59b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      chef_name: "Somchai Supasorn",
-      years_of_experience: 5,
-      number_of_recipes: 50,
-      likes: 100,
-    },
-    {
-      id: 2,
-      chef_picture:
-        "https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-      chef_name: "Nongkran Daks",
-      years_of_experience: 7,
-      number_of_recipes: 75,
-      likes: 200,
-    },
-    {
-      id: 3,
-      chef_picture:
-        "https://images.unsplash.com/photo-1566554273541-37a9ca77b91f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      chef_name: "Saran Sirilak",
-      years_of_experience: 15,
-      number_of_recipes: 120,
-      likes: 300,
-    },
-    {
-      id: 4,
-      chef_picture:
-        "https://images.unsplash.com/photo-1541614101331-1a5a3a194e92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
-      chef_name: "Saran Daks",
-      years_of_experience: 10,
-      number_of_recipes: 90,
-      likes: 150,
-    },
-    {
-      id: 5,
-      chef_picture:
-        "https://images.unsplash.com/photo-1607631568010-a87245c0daf8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      chef_name: "Pim Techawuan",
-      years_of_experience: 3,
-      number_of_recipes: 30,
-      likes: 50,
-    },
-    {
-      id: 6,
-      chef_picture:
-        "https://images.unsplash.com/photo-1595273670150-bd0c3c392e46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1087&q=80",
-      chef_name: "Pim Saran",
-      years_of_experience: 5,
-      number_of_recipes: 30,
-      likes: 500,
-    },
-  ];
+  const chefs = useLoaderData()
+    
 
   return (
     <div className="px-10 grid grid-cols-3 gap-4 pb-10">
       {chefs.map((chef) => (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div key={chef.id} className="card w-96 bg-base-100 shadow-xl">
           <figure>
             <img
               src={chef.chef_picture}
--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -15,7 +15,8 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home></Home>
+                element: <Home></Home>,
+                loader: () => fetch('https://tuk-tuk-kitchen-server-sourovmodak06.vercel.app/')
             },
             {
                 path: '/blog',
