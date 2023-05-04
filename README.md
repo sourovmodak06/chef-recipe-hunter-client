52c37d68d49e2f0d14aaec7021b10ede5d401972
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-04 20:08:41 +0600

    error page and readme file added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tuk Tuk Thai kitchen
+* First the navbar is created with the home blog and login section in mind
+* Then a Carousel slide is created with text on one side and Carousel time set.
+* Next, the chefs are shown an info card that includes their name, work experince and how many dishes they can prepare.
+* Popular dishes are featured in the Amazingly Delicious section
+* Then clicking on the view recipe button will take you to a new page where the food info and some cooking rules are mentioned. If someone doesn't login then he can't come to this page
+* A single click on the add favorite button will disable it
+
+[Live Web site link](https://tuk-tuk-kitchen.web.app/)
--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,16 +4,16 @@ import errorPage from '/images/error-page.png'
 
 const ErrorPage = () => {
     return (
-        <div className='flex justify-between items-center px-32'>
-            <div className='text-[#c5a86b]'>
-                <h2 className='text-6xl font-medium pb-4'>Page Not found</h2>
+        <div className='flex flex-col-reverse md:flex-row justify-between items-center md:px-32'>
+            <div className='text-[#c5a86b] px-5'>
+                <h2 className='text-4xl md:text-6xl font-medium pb-4'>Page Not found</h2>
                 <p className='py-2 font-medium'>We looked everywhere for this page</p>
-                <p className='py-2 font-medium'>Are you sure the website URL is correct?</p>
+                <p className='md:py-2 font-medium'>Are you sure the website URL is correct?</p>
                 <div className='mt-10'>
                     <Link className='py-2 font-medium border-2 px-4 border-red-700 rounded-lg' to='/'>GO Home</Link>
                 </div>
             </div>
-            <img src={errorPage} alt="Error Img" />
+            <img src={errorPage} alt="Error Img"/>
         </div>
     );
 };
